b794f8a85e216f21dd708b053b66358be6c7d8ee
kirillabrosimov <kirillabrosimov@users.noreply.github.com>
2019-02-28 00:49:03 +0300

    Update settings.py

--- my/settings.py
+++ my/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 """
 Django settings for my project.
 
@@ -118,3 +119,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+# Activate Django-Heroku.
+django_heroku.settings(locals())
