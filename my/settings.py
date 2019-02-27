9367713f7d29be6afbbd15a30fb81c8eb9e1dc74
kirillabrosimov <kirillabrosimov@users.noreply.github.com>
2019-02-28 00:38:39 +0300

    Update settings.py

--- my/settings.py
+++ my/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '_(#%q^s@&gr9mr%_iw^ejsmxz#czij+&6$17s!_g^%rt(61dyo'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
