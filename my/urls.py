52c30f8f572e7bbee697c58a9d28abb82cb23f2e
kirillabrosimov <kirillabrosimov@users.noreply.github.com>
2019-03-02 14:05:03 +0300

    Update urls.py

--- my/urls.py
+++ my/urls.py
@@ -17,14 +17,11 @@ from django.contrib import admin
 from django.urls import path
 
 urlpatterns = [
+    def index(request):
+    # return HttpResponse('Hello from Python!')
+    return render(request, "index.html")
+    
     path('admin/', admin.site.urls),
 ]
 
 
-from django.contrib import admin
-from django.urls import include, path
-
-urlpatterns = [
-    path('polls/', include('polls.urls')),
-    path('admin/', admin.site.urls),
-]
